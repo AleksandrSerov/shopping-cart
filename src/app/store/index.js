5141ada0db18545c3615ee794bb64cf68730572a
Aleksandr Serov <sashaserov2010@gmail.com>
2019-05-07 12:32:41 +0300

    Prepare moduules

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,6 +1,13 @@
 import Vue from "vue";
 import Vuex from "Vuex";
+import { product } from "./modules/product";
+import { cart } from "./modules/cart";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({});
+export default new Vuex.Store({
+  modules: {
+    product,
+    cart
+  }
+});
